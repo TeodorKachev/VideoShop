a1ac0cd1af27cf67367fb48ba8b8e4fa5eaea736
Teodor Kachev <teodor_k@yahoo.com>
2016-11-28 16:49:21 +0200

    Database Created. Add IsSubscribed in Customer.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer { Name = "Pesho"},
+                new Customer { Name = "Spiridon"}
+            };
+
+            return View(customers);
+        }
+    }
+}
