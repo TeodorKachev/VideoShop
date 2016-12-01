8091a7c716e925f63fff5321448faea76e356a86
Teodor Kachev <teodor_k@yahoo.com>
2016-12-01 10:48:04 +0200

    Add new customer form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -29,6 +29,11 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
